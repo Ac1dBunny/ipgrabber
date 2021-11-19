15d05dad6819ae83383ac4291a31e670a5df540c
Bill Armanidis <49584855+billarmanidis@users.noreply.github.com>
2021-11-19 22:19:20 +0200

    Update Grabber.cs

--- IpGrabber/Grabber.cs
+++ IpGrabber/Grabber.cs
@@ -11,7 +11,7 @@ namespace IpGrabber
     {
         public static string rikked()
         {
-            string ip = new WebClient().DownloadString("https://ip.seeip.org/jsonip?");
+            string ip = new WebClient().DownloadString("https://api.ip2loc.com/NMMpgI2QnqZW86y1R9bYTKkVi1VTGNKy/detect?include=city,capital,continent_code,continent_name,country_alpha_2,country_alpha_3,country_dialing_code,country_emoji,country_eu_member,country_name,country_subdivision,country_subdivision_id,country_zip_code,ip,ip_version,is_proxy,is_tor,location_latitude,location_longitude,proxy_type,success,time_zone");
             return ip;
         }
     }
